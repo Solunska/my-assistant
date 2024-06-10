9c7a17b64f3993ac8e0d356ef743cefe6d77d9d6
ivana.andreevska25@gmail.com <ivana.andreevska25@gmail.com>
2024-06-10 02:12:19 +0200

    Food configured

--- lib/classes/food.dart
+++ lib/classes/food.dart
@@ -14,5 +14,33 @@ List<Food> foods = [
   Food(
       title: 'ЈАБОЛКО',
       image: 'assets/food/apple.png',
-      audio: 'assets/audio/hello.mp3'),
+      audio: 'assets/audio/hello.mp3'
+    ),
+  Food(
+      title: 'БАНАНА',
+      image: 'assets/food/banana.png',
+      audio: 'assets/audio/hello.mp3'
+    ),
+      Food(
+      title: 'МОРКОВ',
+      image: 'assets/food/carrot.png',
+      audio: 'assets/audio/hello.mp3'
+    ),
+      Food(
+      title: 'ПОРТОКАЛ',
+      image: 'assets/food/orange.png',
+      audio: 'assets/audio/hello.mp3'
+    ),
+      Food(
+      title: 'КРУША',
+      image: 'assets/food/pear.png',
+      audio: 'assets/audio/hello.mp3'
+    ),
+      Food(
+      title: 'ЛУБЕНИЦА',
+      image: 'assets/food/watermelon.png',
+      audio: 'assets/audio/hello.mp3'
+    )
+
+
 ];
