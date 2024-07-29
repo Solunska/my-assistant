c09e5c82ff7f078de3391de36c02145703fc760d
Ivana Andreevska <ivana.andreevska25@gmail.com>
2024-07-29 03:12:11 +0200

    Configured Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:my_assistant/screens/start.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MaterialApp(
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFF084B83),
       ),
       home: const Scaffold(
-        body: StartScreen(), 
+        body: StartScreen(),
       ),
     ),
   );
